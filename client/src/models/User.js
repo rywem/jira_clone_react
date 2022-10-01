36f598a036edf25a895171b9cfa1c1214762d566
Ryan <cybervoid41@gmail.com>
2022-10-01 06:48:13 -0600

    removed old client

--- client/src/models/User.js
+++
@@ -1,7 +0,0 @@
-export class User {
-    constructor(data) {
-        this.username = data.username,
-        this.token = data.token,
-        this.avatarUrl = data.avatarUrl
-    }
-}
--- client/src/pages/NoPage.jsx
+++
@@ -1,5 +0,0 @@
-const NoPage = () => {
-    return <h1>NoPage</h1>;
-};
-
-export default NoPage;
