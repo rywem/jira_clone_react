cce7440d203071ba3e69a1208775ce70912acbfe
Ryan <cybervoid41@gmail.com>
2022-10-09 07:05:18 -0600

    Converted to use Mobx ProjectDashboard, ProjectDetails, ProjectForm

--- jcr-client/src/components/Project/ProjectDashboard.tsx
+++ jcr-client/src/components/Project/ProjectDashboard.tsx
@@ -4,40 +4,34 @@ import ProjectDetails from './ProjectDetails';
 import ProjectList from './ProjectList';
 import { Project } from '../../models/Project';
 import ProjectForm from './ProjectForm';
+import { useStore } from '../../stores/store';
+import { observer } from 'mobx-react-lite';
 
 interface Props {
-    projects: Project[];
-    selectedProject: Project | undefined;
-    selectProject: (id: number) => void;
-    cancelSelectProject: () => void;
-    editMode: boolean;
-    openForm: (id: number) => void;
-    closeForm: () => void;
+    projects: Project[];        
     createOrEdit: (project: Project) => void;
     deleteProject: (id: number) => void;
     submitting: boolean;
 }
 
-export default function ProjectDashboard(props: Props) {
-
-    return (
-        <Grid>
-            <Grid.Column width='10'>                
-                <ProjectList projects={props.projects} selectProject={props.selectProject} deleteProject={props.deleteProject} />
-            </Grid.Column>
-            <Grid.Column width='6'>
-                {props.selectedProject && !props.editMode &&
-                <ProjectDetails project={props.selectedProject} 
-                    cancelSelectProject={props.cancelSelectProject} 
-                    openForm={props.openForm}
-
-                />}                
-                {props.editMode && 
-                <ProjectForm closeForm={props.closeForm} project={props.selectedProject} 
-                    createOrEdit={props.createOrEdit} 
-                    submitting={props.submitting}
-                    />}                
-            </Grid.Column>
-        </Grid>
-    )
-}
+export default observer( function ProjectDashboard(props: Props) {
+        const { projectStore } = useStore();
+        const {selectedProject, editMode } = projectStore; 
+        return (
+            <Grid>
+                <Grid.Column width='10'>                
+                    <ProjectList projects={props.projects} deleteProject={props.deleteProject} />
+                </Grid.Column>
+                <Grid.Column width='6'>
+                    {selectedProject && !editMode &&
+                    <ProjectDetails />}                
+                    {editMode && 
+                    <ProjectForm 
+                        createOrEdit={props.createOrEdit} 
+                        submitting={props.submitting}
+                        />}                
+                </Grid.Column>
+            </Grid>
+        )
+    }
+)
